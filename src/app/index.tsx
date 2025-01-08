e212283b516fb6bb1d357eaaed0f543c5cbc18df
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2025-01-07 23:40:14 -0300

    criando html login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,43 @@
 import Colors from "@/constants/Colors";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 
 export default function Login(){
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text>
+                <Text style={styles.logoText}>
                     Dev<Text style={{color:Colors.green}}>App</Text>
                 </Text>
+                <Text style={styles.slogan}>
+                    o  Futuro da programação
+                </Text>
+
+            </View>
+
+            <View style={styles.form}>
+                <View>
+                    <Text>email</Text>
+                    <TextInput
+                        placeholder="Digite seu email..."
+                        style={styles.input}
+                    
+                    />
+                </View>
+                <View>
+                    <Text style={styles.label}>senha</Text>
+                    <TextInput
+                        placeholder="Digite sua senha..."
+                        style={styles.input}
+                    
+                    />
+                </View>
+
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Entrar
+                    </Text>
+                </Pressable>
 
             </View>
         </View>
@@ -19,8 +48,64 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 34,
-        backgroundColor:Colors.zinc
+        backgroundColor:Colors.zinc 
+    },
+    header: {
+        paddingLeft:14,
+        paddingRight:14,
         
+    },
+    logoText: {
+        fontSize:20,
+        fontWeight:'bold',
+        color:Colors.white,
+        marginBottom:8,
+    },
+    slogan: {
+        fontSize:36,
+        color:Colors.white,
+        marginBottom:34,
+    },
+    label:{
+        color:Colors.zinc,
+        marginBottom:4,
+    },
+    form:{
+        flex:1,
+        backgroundColor:Colors.white,
+        paddingTop:24,
+        paddingLeft:14,
+        paddingRight:14,
+        borderTopLeftRadius:16,
+        borderTopRightRadius:16,
+
+    },
+    input:{
+        borderWidth:1,
+        borderColor:Colors.gray,
+        borderRadius:8,
+        marginBottom:16,
+        paddingHorizontal:8,
+        paddingTop:14,
+        paddingBottom:14,
+
+    },
+    button:{
+        backgroundColor:Colors.green,
+        paddingTop:14,
+        paddingBottom:14,
+        justifyContent:'center',
+        alignItems:"center",
+        width:'100%',
+        borderRadius:8,
+    },
+    buttonText:{
+        color:Colors.zinc,
+        fontSize:16,
+        fontWeight:'bold',
+
     }
+
+
    
 })
